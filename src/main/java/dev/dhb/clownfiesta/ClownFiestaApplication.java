a50d13a8861d3a9854fcd8791dad05ac39913144
digitalhoboxx <stanalin91@gmail.com>
2023-06-12 18:39:11 +0300

    initial commit

---
+++ src/main/java/dev/dhb/clownfiesta/ClownFiestaApplication.java
@@ -0,0 +1,13 @@
+package dev.dhb.clownfiesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClownFiestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClownFiestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dhb/clownfiesta/ClownFiestaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dhb.clownfiesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClownFiestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
