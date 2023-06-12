b6c86923691310d087a0ba362a432adde15c9fb2
digitalhoboxx <stanalin91@gmail.com>
2023-06-12 20:13:41 +0300

    created JwtAuthentication base file

---
+++ src/main/java/dev/dhb/clownfiesta/config/JwtAuthentication.java
@@ -0,0 +1,28 @@
+package dev.dhb.clownfiesta.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthentication extends OncePerRequestFilter {
+
+    //intercepts request and provides new data to response
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            //chain of responsibility design pattern: contains list of other filter that need to be executed
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+}
