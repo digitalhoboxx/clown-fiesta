b70c460d58555f0f661d34afc1c0fb1bf649047e
digitalhoboxx <stanalin91@gmail.com>
2023-06-13 13:42:53 +0300

    implemented authenticate method

--- src/main/java/dev/dhb/clownfiesta/auth/AuthenticationService.java
+++ src/main/java/dev/dhb/clownfiesta/auth/AuthenticationService.java
@@ -5,6 +5,8 @@ import dev.dhb.clownfiesta.user.Role;
 import dev.dhb.clownfiesta.user.User;
 import dev.dhb.clownfiesta.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -32,7 +35,18 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        //at this point email and password are confirmed -> user is authenticated
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
