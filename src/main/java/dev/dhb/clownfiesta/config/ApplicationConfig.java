9fdc259d097002c5b3f73ee03265c9ec0dc98305
digitalhoboxx <stanalin91@gmail.com>
2023-06-13 11:39:13 +0300

    created UserDetails bean

---
+++ src/main/java/dev/dhb/clownfiesta/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package dev.dhb.clownfiesta.config;
+
+import dev.dhb.clownfiesta.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+}
