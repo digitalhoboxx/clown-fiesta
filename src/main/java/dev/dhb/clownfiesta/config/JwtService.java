8479e9430059e4dd9f2e21afb8671eb544b79639
digitalhoboxx <stanalin91@gmail.com>
2023-06-13 10:08:10 +0300

    added method to extract and decode encryption key

--- src/main/java/dev/dhb/clownfiesta/config/JwtService.java
+++ src/main/java/dev/dhb/clownfiesta/config/JwtService.java
@@ -2,10 +2,16 @@ package dev.dhb.clownfiesta.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "436c4f5a7279464a6b775a35332f365769504559766d44763948554f5139616436425864424556412b614c693643527857667454317a5058617438366453416e0a"
     public String extractUsername(String token) {
         return null;
     }
@@ -13,10 +19,16 @@ public class JwtService {
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
+                //signing key is a secret used to digitally sign a jwt; used to create the signature part
                 .setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 }
