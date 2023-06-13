03c9004fd6a78275d3e76916d59ee0254741da7a
digitalhoboxx <stanalin91@gmail.com>
2023-06-13 11:17:26 +0300

    updated extractUsername

--- src/main/java/dev/dhb/clownfiesta/config/JwtService.java
+++ src/main/java/dev/dhb/clownfiesta/config/JwtService.java
@@ -15,7 +15,7 @@ public class JwtService {
     private static final String SECRET_KEY = "436c4f5a7279464a6b775a35332f365769504559766d44763948554f5139616436425864424556412b614c693643527857667454317a5058617438366453416e0a";
 
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
