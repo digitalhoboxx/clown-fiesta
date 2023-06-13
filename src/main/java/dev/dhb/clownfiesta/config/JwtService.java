49c02a4f35d4978c5b08b71e52acd7bad82acd49
digitalhoboxx <stanalin91@gmail.com>
2023-06-13 10:20:04 +0300

    added method to extract claims

--- src/main/java/dev/dhb/clownfiesta/config/JwtService.java
+++ src/main/java/dev/dhb/clownfiesta/config/JwtService.java
@@ -7,13 +7,20 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "436c4f5a7279464a6b775a35332f365769504559766d44763948554f5139616436425864424556412b614c693643527857667454317a5058617438366453416e0a";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
