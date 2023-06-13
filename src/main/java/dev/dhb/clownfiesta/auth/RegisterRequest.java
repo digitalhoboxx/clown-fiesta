1a2fc530f07fd4013bb3be369e51d10e3e782f6c
digitalhoboxx <stanalin91@gmail.com>
2023-06-13 12:59:37 +0300

    created RegisterRequest class

---
+++ src/main/java/dev/dhb/clownfiesta/auth/RegisterRequest.java
@@ -0,0 +1,16 @@
+package dev.dhb.clownfiesta.auth;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+}
